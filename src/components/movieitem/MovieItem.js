daa9f2158605ddd41e58728275ec1cadfee25e3f
tunjiaramide <designteamtechnology@gmail.com>
2021-07-29 04:28:02 +0100

    Completed movie item

--- src/components/movieitem/MovieItem.js
+++ src/components/movieitem/MovieItem.js
@@ -7,7 +7,7 @@ export default function MovieItem() {
                 <div className="img_cover">
                     <img src="https://via.placeholder.com/250-x370" alt="" />
                 </div>
-                <div className="content_cover">
+                <div className={styles.content_cover}>
                     <h2>Ender's Game</h2>
                     <h5>Action</h5>
                     <h6>120 Minutes <span>Stars</span></h6>
@@ -15,7 +15,7 @@ export default function MovieItem() {
                         sed do eiusmod tempor incididunt ut labore et 
                         dolore magna aliqua. </p>
                 </div>
-                <button className="btn btn-primary">Details</button>
+                <button className={`btn ${styles.more}`}>Details</button>
         </div> 
     )
 }
